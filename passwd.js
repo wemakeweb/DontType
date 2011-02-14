bb60f26c8a517291675a888f02651ff8864734e3
wemakeweb <info@wemakeweb.de>
2011-02-14 09:27:26 -0800

    included excanvas support, not working proper right now

--- passwd.js
+++ passwd.js
@@ -1,4 +1,5 @@
-;(function ($, window, document, undefined) {
+;
+(function ($, window, document, undefined) {
 
     $.fn.boomPasswd = function (options) {
         var pluginInstance = $.data(this[0], "boomPasswdInstance"),
@@ -27,41 +28,50 @@
             App.defaults = $.extend({
                 width: 155,
                 height: 155,
-                padding: 20,
-				radius: 14,
+                padding: 25,
+                radius: 14,
                 styles: {
                     circle: {
                         borderGradient: {
                             from: '#313131',
-                            to: '#232323',
+                            to: '#232323'
                         },
-                        innerColor: '#fff',
+                        innerColor: '#fff'
                     },
                     line: {
-                        color: '#fff',
-                    },
+                        color: '#fff'
+                    }
                 },
 
                 //advanced options
                 minDirectionChange: 1,
                 minConnections: 3,
-				convertMap : [1,2,3,4,5,6,7,8,9],
-				reParse : false,
+                convertMap: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                reParse: false,
 
                 // callbacks
                 inputEnd: function (input) {},
                 inputStart: function () {},
+                noSupport: function () {
+                    // include ExCanvas	 locally
+                    var s = document.createElement('script');
+                    s.src = 'excanvas.js';
+                    s.type = 'text/javascript';
+                    document.getElementsByTagName("head")[0].appendChild(s)
+                }
 
             }, options);
 
             if (App.checkSupport()) {
-              	App.input = $(elem)//.hide();
-                App.setup($(elem));
+                App.input = $(elem).hide();
+                App.setup(App.input)
             } else {
-                // include mordernizer
+                App.defaults.noSupport.call()
+                App.noSupport = true;
+                App.input = $(elem).hide();
+                App.setup(App.input);
             }
-			
-			//this.grid();
+
             // Return the new password field
             //return self.$field[0];
         },
@@ -69,35 +79,36 @@
         hittedPoints: [],
 
         points: [],
-		
-		checkSupport: function () {
+
+        checkSupport: function () {
             return !!document.createElement('canvas').getContext;
         },
-		
-		updateInput : function(){
-			var passwd = '', App = this;
-			$.each( App.hittedPoints, function(i,val){
-				passwd += App.defaults.convertMap[val[1]*3 + val[0]]
-			});
-			
-			$(this.input).val(passwd);	
-		},
-		
+
+        updateInput: function () {
+            var passwd = '',
+                App = this;
+            $.each(App.hittedPoints, function (i, val) {
+                passwd += App.defaults.convertMap[val[1] * 3 + val[0]]
+            });
+
+            $(this.input).val(passwd);
+        },
+
         setup: function (field) {
-            // cache
             var App = this,
-                ctx = document.createElement('canvas');					
-				ctx.width = App.defaults.width;
-				ctx.height = App.defaults.height;
-				ctx.id = 'boomPasswdCtx';			
-				
-            $(ctx, {
-                mousedown: App.uiEvent,
-                mouseup: App.uiEvent,
-                mousemove: App.uiEvent,
-                mouseout: App.uiEvent,
-            }).data({
-                'boomPasswdInstance': App,
+                canvas = document.createElement('canvas'),
+                ctx = false;
+            canvas.width = App.defaults.width;
+            canvas.height = App.defaults.height;
+            canvas.id = 'boomPasswdCtx';
+            $(field).parent().append(canvas)
+
+            if (!document.createElement('canvas').getContext) {
+                ctx = G_vmlCanvasManager.initElement(canvas);
+            }
+
+            $(document.getElementById('boomPasswdCtx')).data({
+                'boomPasswdInstance': App
             }).bind('mousedown mouseup mousemove mouseout', function (event) {
                 var x = event.pageX - this.offsetLeft,
                     y = event.pageY - this.offsetTop;
@@ -105,7 +116,12 @@
                 switch (event.type) {
                 case 'mousedown':
                     App.mousedown = true;
-                    App.start(x, y);
+
+                    if (App.hittedPoints.length) {
+                        App.connect(x, y)
+                    } else {
+                        App.start(x, y);
+                    }
                     break;
                 case 'mouseup':
                 case 'mouseout':
@@ -115,20 +131,23 @@
                     App.mousedown && App.connect(x, y);
                     break;
                 }
-            }).appendTo($(field).parent());
+            });
 
-            ctx = App.ctx = document.getElementById('boomPasswdCtx').getContext('2d');
-			
-            App.pixelSteps = Math.floor((App.defaults.width - (App.defaults.padding)) / 3);
-console.log(App.pixelSteps);
+            if (!ctx) {
+                ctx = document.getElementById('boomPasswdCtx');
+            }
+            App.ctx = ctx = ctx.getContext('2d');
+
+            App.pixelSteps = Math.floor((App.defaults.width) / 3);
 
             // calculate point cords
             $.each([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]], function (i, v) {
-                App.points.push([App.defaults.padding + App.pixelSteps * v[1], App.defaults.padding + App.pixelSteps * v[0]])
+                App.points.push([App.defaults.padding + (App.pixelSteps * v[1]), App.defaults.padding + (App.pixelSteps * v[0])])
+
             });
 
             // draw point base 
-     		$.each(App.points, function (i, point) {
+            $.each(App.points, function (i, point) {
                 ctx.beginPath();
                 ctx.arc(point[0], point[1], App.defaults.radius, 0, 360, false);
 
@@ -145,29 +164,22 @@ console.log(App.pixelSteps);
                 ctx.fillStyle = 'white';
                 ctx.fill();
             });
-			
+
         },
 
         active: function (point) {
-            var ctx = this.ctx;
+            var App = this,
+                ctx = App.ctx;
             ctx.globalCompositeOperation = 'destination-over';
             ctx.beginPath();
-            ctx.arc(this.defaults.padding + point[0] * this.pixelSteps, this.defaults.padding + point[1] * this.pixelSteps, this.defaults.radius + 2, 0, 360, false);
+            ctx.arc(App.defaults.padding + point[0] * App.pixelSteps, App.defaults.padding + point[1] * App.pixelSteps, App.defaults.radius + 2, 0, 360, false);
             ctx.fillStyle = "#7bd93e";
             ctx.fill();
-			
+
             ctx.beginPath();
-            ctx.arc(this.defaults.padding + point[0] * this.pixelSteps, this.defaults.padding + point[1] * this.pixelSteps, this.defaults.radius + 3, 0, 360, false);
+            ctx.arc(App.defaults.padding + point[0] * App.pixelSteps, App.defaults.padding + point[1] * App.pixelSteps, App.defaults.radius + 3, 0, 360, false);
             ctx.fillStyle = "#acacac";
             ctx.fill();
-		},
-
-        grid: function () {
-            for (var y = 0; y <= 2; y++) {
-                for (var x = 0; x <= 2; x++) {
-                    this.ctx.strokeRect(this.pixelSteps * x  + this.pixelSteps*0.1, this.pixelSteps * y + this.pixelSteps*0.1, this.pixelSteps-this.pixelSteps*0.3, this.pixelSteps - this.pixelSteps*0.3);
-                }
-            }
         },
 
         notUsed: function (point) {
@@ -184,24 +196,25 @@ console.log(App.pixelSteps);
             var pixelSteps = this.pixelSteps,
                 xF = Math.floor(x / pixelSteps),
                 yF = Math.floor(y / pixelSteps),
-				point = [ this.defaults.padding + xF * pixelSteps, this.defaults.padding + yF * pixelSteps],
-				rad = this.defaults.radius;
-				
-				// debug: this.ctx.strokeRect(point[0] - this.defaults.radius, point[1] - this.defaults.radius ,2*this.defaults.radius, 2* this.defaults.radius );				
-				return (x> (point[0]-rad) && x < (point[0]+rad) && y > (point[1]-rad) && y < (point[1]+rad) && x < this.defaults.width && y < this.defaults.height && this.notUsed([xF, yF])) ?					
-						[xF, yF] : false;
-						
+                point = [this.defaults.padding + xF * pixelSteps, this.defaults.padding + yF * pixelSteps],
+                rad = this.defaults.radius;
+
+            // debug: this.ctx.strokeRect(point[0] - this.defaults.radius, point[1] - this.defaults.radius ,2*this.defaults.radius, 2* this.defaults.radius );				
+            return (x > (point[0] - rad) && x < (point[0] + rad) && y > (point[1] - rad) && y < (point[1] + rad) && x < this.defaults.width && y < this.defaults.height && this.notUsed([xF, yF])) ? [xF, yF] : false;
+
         },
         connect: function (x, y) {
-            var point = this.hit(x, y),
-                ctx = this.ctx,
-                points = this.hittedPoints,
+            var App = this,
+                point = App.hit(x, y),
+                ctx = App.ctx,
+                points = App.hittedPoints,
                 len = points.length;
 
             if ( !! point) {
+                this.crossing();
                 this.active(point);
                 this.hittedPoints.push(point);
-				this.updateInput();
+                this.updateInput();
 
                 //draw connection line
                 ctx.globalCompositeOperation = 'source-over';
@@ -209,10 +222,10 @@ console.log(App.pixelSteps);
                 ctx.strokeStyle = 'rgba(207,207,207,0.85)';
                 ctx.lineCap = 'round';
                 ctx.lineWidth = 14;
-                ctx.moveTo(this.defaults.padding + points[len - 1][0] * this.pixelSteps, this.defaults.padding + points[len - 1][1] * this.pixelSteps);
-                ctx.lineTo(this.defaults.padding + points[len][0] * this.pixelSteps, this.defaults.padding + points[len][1] * this.pixelSteps);
+                ctx.moveTo(App.defaults.padding + points[len - 1][0] * App.pixelSteps, App.defaults.padding + points[len - 1][1] * App.pixelSteps);
+                ctx.lineTo(App.defaults.padding + points[len][0] * App.pixelSteps, App.defaults.padding + points[len][1] * App.pixelSteps);
                 ctx.stroke();
-				
+
 
             }
         },
@@ -223,10 +236,15 @@ console.log(App.pixelSteps);
 
             // if hit && not used yet
             if ( !! point) {
+                this.crossing();
                 this.active(point)
                 this.hittedPoints.push(point);
-				this.updateInput();
+                this.updateInput();
             }
+        },
+
+        crossing: function () {
+
         },
     };
     boomPasswd.init.prototype = boomPasswd;
